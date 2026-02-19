9ac5ae2462dc883115ae5218c9adb80fe4d5109b
BesimaP <bepl1000@stud.ek.dk>
2026-02-19 11:15:19 +0100

    assignment 1,2,3 done

--- Øvelser/uge_opgave_4/src/Assignment_1/Student.java
+++ Øvelser/uge_opgave_4/src/Assignment_1/Student.java
@@ -1,15 +1,16 @@
-    public class Student {
+package Assignment_1;
+
+public class Student {
         String name;
         int age;
 
-         Student (String name, int age){
+        Student (String name, int age){
             this.name = name;
             this.age = age;
         }
 
 
-         void printInfo(){
+        void printInfo(){
             System.out.println("Student name: " + name + ". " + "Age: " + age);
         }
-
-    }
+}
---
+++ Øvelser/uge_opgave_4/src/Assignment_4/Player.java
@@ -0,0 +1,11 @@
+package Assignment_4;
+
+    public class Player {
+        String name;
+        int skillLevel;
+
+        public Player(String name, int skillLevel){
+            this.name = name;
+            this.skillLevel = skillLevel;
+        }
+    }
